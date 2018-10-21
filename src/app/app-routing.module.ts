510c61c29541b096b12d48b5c719a9501114b5db
Manuel Prieto Rivas <mepr98>
2018-10-20 20:03:18 -0400

    listorouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule, Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import{HomepageComponent} from './componentes/homepage/homepage.component'
-import{LoginComponent} from './componentes/login/login.component'
-import{AdminComponent} from './componentes/admin/admin.component'
+import {AppComponent} from '../app/app.component';
+import {HomepageComponent} from './componentes/homepage/homepage.component';
+import {LoginComponent} from './componentes/login/login.component';
+import {AdminComponent} from './componentes/admin/admin.component';
+import {CambioComponent} from './componentes/cambio/cambio.component';
+import {ComprasComponent} from './componentes/compras/compras.component';
+import {OrdenComponent} from './componentes/orden/orden.component';
 const routes: Routes = [
-  {path:'',component: HomepageComponent},
+  {path:'home',component: HomepageComponent},
   {path:'login',component:LoginComponent},
-  {path:'admin',component:AdminComponent}
+  {path:'admin',component:AdminComponent},
+  {path:'cambio',component:CambioComponent},
+  {path:'compras',component:ComprasComponent},
+  {path:'orden',component:OrdenComponent},
   
 ];
 
