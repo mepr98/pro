ed88fc6f45dd3b78892ae337a05951dad873f020
System Administrator <root@Victors-MacBook-Pro.local>
2018-10-24 17:11:17 -0400

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import {CambioComponent} from './componentes/cambio/cambio.component';
 import {ComprasComponent} from './componentes/compras/compras.component';
 import {OrdenComponent} from './componentes/orden/orden.component';
 const routes: Routes = [
-  {path:'home',component: HomepageComponent},
+  {path:'',component: HomepageComponent},
   {path:'login',component:LoginComponent},
   {path:'admin',component:AdminComponent},
   {path:'cambio',component:CambioComponent},
